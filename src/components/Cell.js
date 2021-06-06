92fcc9028e17c660031644a529c19edfc6bb07b4
LiewShanWei <shanweiliew93@gmail.com>
2021-06-06 11:48:06 +0800

    Enhance: Cell css Feature: Cell now changes color on click by changing class from cell to wall

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,6 +1,23 @@
+import { useState } from 'react';
+import styles from './Cell.module.css';
+
 function Cell(){
+    const [isWall, setIsWall] = useState(false);
+
+    function onCellClickHandler(){
+        setIsWall(true);
+    };
+
     return (
-        <td>test</td>
+        <td 
+            onClick={onCellClickHandler} 
+            className={ 
+                (!isWall && styles.cell) || 
+                (isWall && styles.wall) 
+            }
+        >
+
+        </td>
     );
 };
 
