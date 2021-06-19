eca0de5ec200529bef1fbe30c6fe76fcb49b064a
LiewShanWei <shanweiliew93@gmail.com>
2021-06-19 21:10:09 +0800

    Updated cells Added visited animation to .css file Attempted to animate visited animation whilst traversing

--- src/components/Table/Cell.js
+++ src/components/Table/Cell.js
@@ -1,16 +1,43 @@
-import React, { useState, forwardRef } from 'react';
+import React, { useState } from 'react';
 import styles from './Cell.module.css';
 
-const Cell = React.forwardRef((props,ref) => {
+const Cell = (props) => {
     const [isWall, setIsWall]= useState(false);
+    const [isVisited, setIsVisited] = useState(false);
 
-    let isStartCellClass = `${props.startCell === props.id ? styles.start : '' }`;
-    let isEndCellClass = `${props.endCell === props.id ? styles.end : '' }`;
-    let isNeitherCellType = `${!isStartCellClass && !isEndCellClass ? styles.cell : '' }`
-    let isWallCellType = `${isWall ? styles.wall : ''}`;
-    let combinedCellClass = `${isStartCellClass} ${isEndCellClass} ${isWallCellType} ${isNeitherCellType}`;
+    const generateCellClass = () => {
+        console.log("Cell.generateCellClass");
+        if(!isVisited && isInVisitedArray()){
+            setIsVisited(true);
+            props.updateCellVisited(props.id);
+            return `${styles.cell} ${styles.visited}`;
+        }
+
+        if(props.id === props.startCell)
+            return `${styles.cell} ${styles.start}`;    
+        else if (props.id === props.endCell)
+            return `${styles.cell} ${styles.end}`;    
+        else if (isWall)
+            return `${styles.cell} ${styles.wall}`;
+        else
+            return `${styles.cell}`;
+    }
+
+    const isInVisitedArray = () => {
+        const isIn =  props.visitedCells.find((element) => { return element === props.id;}) !== undefined;
+        console.log("Cells.isIn: " + props.id + " " + isIn);
+        return isIn;
+    }
+
+    // let isStartCellClass = `${props.startCell === props.id ? styles.start : '' }`;
+    // let isEndCellClass = `${props.endCell === props.id ? styles.end : '' }`;
+    // let isNeitherCellType = `${!isStartCellClass && !isEndCellClass ? styles.cell : '' }`
+    // let isWallCellType = `${isWall ? styles.wall : ''}`;
+    // let combinedCellClass = `${isStartCellClass} ${isEndCellClass} ${isWallCellType} ${isNeitherCellType}`;
     
     const onCellClickHandler = (event) => {
+        console.log("Cell.onCellClickHandler: " + event.target.id);
+        console.log(props.selectedCellType);
         if(props.selectedCellType === 'start' || props.selectedCellType === 'end'){
             setIsWall(false);
             props.onCellClick(event.target.id);
@@ -25,14 +52,13 @@ const Cell = React.forwardRef((props,ref) => {
 
     return (
         <td 
-            ref={ref}
             id={props.id}
-            className={combinedCellClass}
+            className={generateCellClass()}
             onClick={onCellClickHandler}
         >
             {props.cellCount}
         </td>
     );
-});
+};
 
 export default Cell;
--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -7,11 +7,12 @@ const Table = (props) => {
     const [walls,setWalls] = useState([]);
     const [startCell,setStartCell] = useState();
     const [endCell, setEndCell] = useState();
-    const [animationQueue, setAnimationQueue] = useState([]);
+    //const [animationQueue, setAnimationQueue] = useState([]);
     const [visitedCells , setVisitedCells] = useState([]);
+    // let visitedCells = [];
 
-    const rowCount = 2;
-    const colCount = 3;
+    const rowCount = 10;
+    const colCount = 10;
     const totalCellCount = rowCount * colCount;
     const cellRefs = useRef(new Array(totalCellCount).map(() => createRef()));
     
@@ -39,20 +40,20 @@ const Table = (props) => {
     //Generates table html 
     const generateTableCells = () => {
         console.log("Table.generateTableCells() running");
-        let cellCount = 0;
-        let cellArray = [];
+        // let cellCount = 0;
+        //let cellArray = [];
         let rowsHtml = [];
         for( var r = 0; r<rowCount;r++){
-            let currentRowArray = [];
+            //let currentRowArray = [];
             let cellsHtml = [];
             for( var c = 0; c<colCount; c++){
                 let cellId = `${r}-${c}`
-                cellsHtml.push(generateDefaultCell(cellId,cellCount));
-                currentRowArray.push(cellId);
-                cellCount++;
+                cellsHtml.push(generateDefaultCell(cellId));
+                //currentRowArray.push(cellId);
+                // cellCount++;
             }
             rowsHtml.push(generateDefaultRow(r,cellsHtml))
-            cellArray.push(currentRowArray);
+            //cellArray.push(currentRowArray);
         }
         return rowsHtml;
     };
@@ -71,10 +72,10 @@ const Table = (props) => {
     };
 
     //Generates <Cell> html
-    const generateDefaultCell = (cellId, cellCount) => {
+    const generateDefaultCell = (cellId) => {
         return (
             <td 
-                ref={(el) => cellRefs.current[cellCount] = el}
+                // ref={(el) => cellRefs.current[cellCount] = el}
                 key={cellId} 
                 id={cellId} 
                 className={generateCellClass(cellId)}
@@ -82,17 +83,31 @@ const Table = (props) => {
             >
                 {cellId}
             </td>
+            // <Cell
+            //     key={cellId}
+            //     id={cellId}
+            //     startCell={startCell}
+            //     endCell={endCell}
+            //     selectedCellType = {props.currentSelectedCellType}
+            //     onCellClick={onCellClickHandler}
+            //     cellCount={cellCount}
+            //     visitedCells={visitedCells}
+            // >
+
+            // </Cell>
         );
     }
     const generateCellClass = (cellId) => {
         console.log("Generating cell class: " + cellId);
+        if(isCellAlreadyVisited(cellId))
+            return `${styles.cell} ${styles.visited}`;
+
         if(cellId === startCell)
             return `${styles.cell} ${styles.start}`;    
         else if (cellId === endCell)
             return `${styles.cell} ${styles.end}`;    
-        else if (isInWallArray(cellId)){
-            return `${styles.cell} ${styles.wall}`;   
-        } 
+        else if (isInWallArray(cellId))
+            return `${styles.cell} ${styles.wall}`;
         else
             return `${styles.cell}`;
     }
@@ -154,25 +169,43 @@ const Table = (props) => {
     };
 
     //Helper function for modifying animation queue state
-    const addToAnimationQueue = (cellId) => {
-        setAnimationQueue(previousState => (
-            [...previousState,cellId]
-        ));
-    };
-    const removeFirstFromAnimationQueue = (cellId) => {
-        setAnimationQueue(previousState => {
-            previousState.slice(1);
-        })
-    }
+    // const addToAnimationQueue = (cellId) => {
+    //     setAnimationQueue(previousState => (
+    //         [...previousState,cellId]
+    //     ));
+    // };
+    // const removeFirstFromAnimationQueue = () => {
+    //     setAnimationQueue(previousState => {
+    //         previousState.slice(1);
+    //     })
+    // }
+    // const isInAnimationQueue = (element) => {
+    //     if(animationQueue !== undefined && animationQueue.length > 0){
+    //         return animationQueue.find((curr) => { return curr === element;}) !== undefined;
+    //     }
+            
+    //     return false;
+    // }
 
     //Helper function for modifing visited cells state
     const addToVisitedCells = (cellId) => {
-        setVisitedCells(previousState => (
-            [...previousState, cellId]
-        ))
-    }
-    const isCellAlreadyVisited = (visitedCells, cellId) => {
+        if(!isCellAlreadyVisited(cellId)){
+            setVisitedCells(previousState => (
+                [...previousState, cellId]
+            ));
+            // visitedCells.push(cellId);
+        };
+    };
+    // const isInVisitedCells = (cellId) => {
+    //     if(visitedCells !== undefined && visitedCells.length > 0){
+    //         return visitedCells.find((element) => { return element === cellId;}) !== undefined;
+    //     }
+            
+    //     return false;
+    // }
+    const isCellAlreadyVisited = (cellId) => {
         if(visitedCells !== undefined && visitedCells.length > 0){
+            console.log(visitedCells);
             const isFound = visitedCells.find((element) => { return element === cellId;}) 
             if(isFound){
                 console.log("Already visited: " + cellId);
@@ -203,25 +236,15 @@ const Table = (props) => {
                 //todo: Insert modal informing about missing start/end cell
                 return;
             }
-            //printCellsHelper();
-            console.log("cells");
-            console.log(cells);
-            console.log("startCell");
-            console.log(startCell);
-            console.log("endCell");
-            console.log(endCell);
-            console.log("walls");
-            console.log(walls);
-            console.log("refs");
-            console.log(cellRefs);
             
-            let visitedCells = [];
-            visitedCells.push(startCell);
-            let animationQueue = [];
-            animationQueue.push(startCell);
+            addToVisitedCells(startCell);
+            // visitedCells.push(startCell);
+            let neighbourQueue = [];
+            neighbourQueue.push(startCell);
 
-            while(animationQueue.length > 0){
-                var cellIdParent = animationQueue.pop();
+            while(neighbourQueue.length > 0){
+                console.log("Queue has items");
+                var cellIdParent = neighbourQueue.pop();
                 var rowPos = cellIdParent.split("-")[0];
                 var colPos = cellIdParent.split("-")[1];
 
@@ -235,40 +258,63 @@ const Table = (props) => {
                 var childRightId = rowPos + "-" + colRight;
                 var childBottomId = rowBelow + "-" + colPos;
 
-                if(isCellInBoard(rowAbove,colPos) && !isCellAlreadyVisited(visitedCells,childTopId)){
-                    const cellIndex = getCellIndex(rowAbove,colPos);
-                    var element = cellRefs.current[cellIndex];
-                    visitedCells.push(childTopId);
-                    animationQueue.push(childTopId);
-                    console.log("Pushed top child to queue: " + childTopId);
+                if(isCellInBoard(rowAbove,colPos) && !isCellAlreadyVisited(childTopId) && !isInWallArray(childTopId)){
+                    // const cellIndex = getCellIndex(rowAbove,colPos);
+                    // var element = cellRefs.current[cellIndex];
+                    neighbourQueue.push(childTopId);
+                    //addToAnimationQueue(childTopId);
+                    // visitedCells.push(childTopId);
+                    addToVisitedCells(childTopId);
+
+                    if(childTopId === endCell)
+                        return;
                 }
-                if(isCellInBoard(rowBelow,colPos) && !isCellAlreadyVisited(visitedCells,childBottomId)){
-                    const cellIndex = getCellIndex(rowBelow,colPos);
-                    var element = cellRefs.current[cellIndex];
-                    visitedCells.push(childBottomId);
-                    animationQueue.push(childBottomId);
-                    console.log("Pushed bottom child to queue: " + childBottomId);
+                if(isCellInBoard(rowBelow,colPos) && !isCellAlreadyVisited(childBottomId) && !isInWallArray(childBottomId)){
+                    // const cellIndex = getCellIndex(rowBelow,colPos);
+                    // var element = cellRefs.current[cellIndex];
+                    neighbourQueue.push(childBottomId);
+                    // addToAnimationQueue(childBottomId);
+                    // visitedCells.push(childBottomId);
+                    addToVisitedCells(childBottomId);
+
+                    if(childBottomId === endCell)
+                        return;
                 }
-                if(isCellInBoard(rowPos,colLeft) && !isCellAlreadyVisited(visitedCells,childLeftId)){
-                    const cellIndex = getCellIndex(rowPos,colLeft);
-                    var element = cellRefs.current[cellIndex];
-                    visitedCells.push(childLeftId);
-                    animationQueue.push(childLeftId);
-                    console.log("Pushed left child to queue: " + childLeftId);
+                if(isCellInBoard(rowPos,colLeft) && !isCellAlreadyVisited(childLeftId) && !isInWallArray(childLeftId)){
+                    // const cellIndex = getCellIndex(rowPos,colLeft);
+                    // var element = cellRefs.current[cellIndex];
+                    neighbourQueue.push(childLeftId);
+                    // addToAnimationQueue(childLeftId);
+                    // visitedCells.push(childLeftId);
+                    addToVisitedCells(childLeftId);
+
+                    if(childLeftId === endCell)
+                        return;
                 }
-                if(isCellInBoard(rowPos,colRight) && !isCellAlreadyVisited(visitedCells,childRightId)){
-                    const cellIndex = getCellIndex(rowPos,colRight);
-                    var element = cellRefs.current[cellIndex];
-                    visitedCells.push(childRightId);
-                    animationQueue.push(childRightId);
-                    console.log("Pushed right child to queue: " + childRightId);
+                if(isCellInBoard(rowPos,colRight) && !isCellAlreadyVisited(childRightId) && !isInWallArray(childRightId)){
+                    // const cellIndex = getCellIndex(rowPos,colRight);
+                    // var element = cellRefs.current[cellIndex];
+                    neighbourQueue.push(childRightId);
+                    // addToAnimationQueue(childRightId);
+                    // visitedCells.push(childRightId);
+                    addToVisitedCells(childRightId);
+
+                    if(childRightId === endCell)
+                        return;
                 }
             }
         }
     }, [props.startVisualization]);
 
+    // useEffect(() => {
+    //     while(animationQueue.length > 0 ){
+    //         var element = animationQueue.pop();
+    //         element.s
+    //     }
+    // }, [animationQueue])
+
     const isCellInBoard = (rowPos, colPos) => {
-        console.log("Is this cell in board: " + rowPos + " " + colPos);
+        //console.log("Is this cell in board: " + rowPos + " " + colPos);
         const validRow = rowPos >=0 && rowPos < rowCount;
         const validCol = colPos >=0 && colPos < colCount;
         return validRow && validCol;
@@ -283,10 +329,12 @@ const Table = (props) => {
         console.log(endCell);
         console.log("walls");
         console.log(walls);
-        console.log("animationQueue");
-        console.log(animationQueue);
+        // console.log("animationQueue");
+        // console.log(animationQueue);
         console.log("visitedCells");
         console.log(visitedCells);
+        console.log("cellRefs");
+        console.log(cellRefs);
     }
 
     return (
